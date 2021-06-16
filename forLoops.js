deab5036f52e258924ad31c9258955d7a04bc17e
Suvij Surya <suvijsurya76@gmail.com>
2021-06-16 17:36:59 +0530

    Create forLoops.js

---
+++ forLoops.js
@@ -0,0 +1,10 @@
+const files = ['a','b','c'];
+let filePaths = [];
+
+for (let file of files) {
+  const fileName = file.trim();
+  if(fileName) {
+    const filePath = `~/cool_app/${fileName}`;
+    filePaths.push(filePath);
+  }
+}
